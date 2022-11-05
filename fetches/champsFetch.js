b4875974f445e93f6a9525eb5aeed2a4fe5f7344
Danny Pellegrini <danny.m.pellegrini@gmail.com>
2022-11-04 20:24:59 -0400

    Finalized character fetch image made JSON file

--- fetches/champsFetch.js
+++ fetches/champsFetch.js
@@ -5,27 +5,24 @@ import axios from 'axios'
 
 const charsURL = 'http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json'
 
-// Fetches Character Names and Images
-async function fetchAllChars(url){
-  const characters = []
-  const response = await axios(url)
-  const allChars = await response.data.data
-  for (let char in allChars) {
-    characters.push(await fetchCharacter(char))
-  }
-  console.log(characters)
-  return characters  
-}
 
 // Fetches Individual Character data
 async function fetchCharacter(charName) {
   const charURL = `http://ddragon.leagueoflegends.com/cdn/12.21.1/data/en_US/champion/${charName}.json`
+  const imgLoadURL = `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/`
+  const imgSplashURL = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/`
+  const imgSquareURL = `http://ddragon.leagueoflegends.com/cdn/12.21.1/img/champion/`
+  const imgPassiveURL = `http://ddragon.leagueoflegends.com/cdn/12.21.1/img/passive/`
+  const imgSpellURL = `http://ddragon.leagueoflegends.com/cdn/12.21.1/img/spell/`
   const response = await axios(charURL)
   const charData = response.data.data
   const { name, title, image, tags, partype, lore, blurb, spells, passive } = charData[charName]
   const character = {
     name: name,
     title: title,
+    image_loading: imgLoadURL + charName + `_0.jpg`,
+    image_splash: imgSplashURL + charName + `_0.jpg`,
+    image_square: imgSquareURL + charName + `.png`,
     tags: tags,
     partype: partype,
     lore: lore,
@@ -35,36 +32,52 @@ async function fetchCharacter(charName) {
         id: spells[0].id,
         name: spells[0].name,
         description: spells[0].description,
-        image_sprite: spells[0].image.sprite
+        image_sprite: imgSpellURL + spells[0].id + `.png`
       },
       {
         id: spells[1].id,
         name: spells[1].name,
         description: spells[1].description,
-        image_sprite: spells[1].image.sprite
+        image_sprite: imgSpellURL + spells[1].id + `.png`
       },
       {
         id: spells[2].id,
         name: spells[2].name,
         description: spells[2].description,
-        image_sprite: spells[2].image.sprite
+        image_sprite: imgSpellURL + spells[2].id + `.png`
       },
       {
         id: spells[3].id,
         name: spells[3].name,
         description: spells[3].description,
-        image_sprite: spells[3].image.sprite
+        image_sprite: imgSpellURL + spells[3].id + `.png`
       }
     ],
     passive: {
       name: passive.name,
       description: passive.description,
-      image_sprite: passive.image.sprite 
+      image_sprite: imgPassiveURL + name + `_Passive.png` 
     }
   }
-  // console.log(character)
   return character
 }
 
+// Fetches All Character Data and Makes JSON File
+async function fetchAllChars(url){
+  const characters = []
+  const response = await axios(url)
+  const allChars = await response.data.data
+  for (let char in allChars) {
+    const character = await fetchCharacter(char)
+    characters.push(character)
+    console.log(`Retrieved ${character.name}`)
+  }
+  await fsPromises.writeFile("champs.json", JSON.stringify(characters))
+  return characters  
+}
+
 fetchAllChars(charsURL)
+
+
+
 
