bf807287ac0f0d31395815977c32fe8a204af47b
Victor Apaez <vic.apaez@gmail.com>
2022-11-08 15:58:23 -0500

    changed password to password_digest

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   userName: String,
-  passWord: String,
-  favItems: [{type: mongoose.Schema.Types.ObjectId, ref: "favItems"}],
-  favCharacters: [{type: mongoose.Schema.Types.ObjectId, ref: "favCharacters"}],
-})
+  password_digest: String,
+  favItems: [{ type: mongoose.Schema.Types.ObjectId, ref: "favItems" }],
+  favCharacters: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "favCharacters" },
+  ],
+});
 
-export default mongoose.model('User', userSchema)
-
+export default mongoose.model("User", userSchema);
