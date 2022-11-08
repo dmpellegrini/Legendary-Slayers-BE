db2126e5488e6e85cf1e187da360547bcdb71f81
Victor Apaez <vic.apaez@gmail.com>
2022-11-08 17:06:58 -0500

    fixed ref in user schema

--- models/User.js
+++ models/User.js
@@ -3,10 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   userName: String,
   password_digest: String,
-  favItems: [{ type: mongoose.Schema.Types.ObjectId, ref: "favItems" }],
-  favCharacters: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "favCharacters" },
-  ],
+  favItems: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
+  favCharacters: [{ type: mongoose.Schema.Types.ObjectId, ref: "Champion" }],
 });
 
 export default mongoose.model("User", userSchema);
