221bab7c14d1c8fdfd11012503c24cdf9a2177e8
Victor Apaez <vic.apaez@gmail.com>
2022-11-08 14:51:18 -0500

    changed create user endpoint to be /signUp and imported jwt aand bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,56 @@
-import { Router } from "express"
-import User from "../models/User.js"
+import { Router } from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const router = Router();
 
-const router = Router()
+// for development purposes
+let SALT_ROUNDS = 11;
+let TOKEN_KEY = "areallylonggoodkey";
 
-router.get("/", (req, res) => res.send("This is the Users Root"))
+// for production
+if (process.env.NODE_ENV === "production") {
+  SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
+  TOKEN_KEY = process.env.TOKEN_KEY;
+}
+
+// for JWT expiration
+const today = new Date();
+const exp = new Date(today);
+exp.setDate(today.getDate() + 30);
+
+router.get("/", (req, res) => res.send("This is the Users Root"));
 
 router.get("/all", (req, res) => {
-  User.find({}) 
-    .then(users => res.json(users))
-})
+  User.find({}).then((users) => res.json(users));
+});
 
 router.get("/name/:name", (req, res) => {
-  User.find({userName: req.params.name}) 
-    .then(user => res.json(user))
-})
+  User.find({ userName: req.params.name }).then((user) => res.json(user));
+});
 
-router.post("/", (req, res) => {
-  User.create(req.body)
-    .then(user => res.json(user))
-})
+router.post("/signUp", (req, res) => {
+  User.create(req.body).then((user) => res.json(user));
+});
 
 router.delete("/name/:name", (req, res) => {
-  User.deleteOne({userName: req.params.name})
-    .then(user => res.json(user))
-})
+  User.deleteOne({ userName: req.params.name }).then((user) => res.json(user));
+});
 
 router.put("/name/:name/champions/:id", (req, res) => {
-  User.findOne({userName: req.params.name})
-  .then(user => {
-    user.favCharacters.push(req.params.id)
-    user.save()
-    res.json(user)
-  })
-})
+  User.findOne({ userName: req.params.name }).then((user) => {
+    user.favCharacters.push(req.params.id);
+    user.save();
+    res.json(user);
+  });
+});
 
 router.put("/name/:name/items/:id", (req, res) => {
-  User.findOne({userName: req.params.name})
-  .then(user => {
-    user.favItems.push(req.params.id)
-    user.save()
-    res.json(user)
-  })
-})
+  User.findOne({ userName: req.params.name }).then((user) => {
+    user.favItems.push(req.params.id);
+    user.save();
+    res.json(user);
+  });
+});
 
-export default router
+export default router;
