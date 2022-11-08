a2f0608ea1136107a03fff6435e8bb435e40ae64
Daniele Pellegrini <danny.m.pellegrini@gmail.com>
2022-11-08 00:35:21 -0500

    Added full CRUD to users db

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,34 @@
 import { Router } from "express"
+import User from "../models/User.js"
 
 const router = Router()
 
 router.get("/", (req, res) => res.send("This is the Users Root"))
 
-// app.get("/Champions", (req,res) => {
-//
-//
-//   Champion.find({})
-//     .then(champions => res.json(champions))
-// })
+router.get("/all", (req, res) => {
+  User.find({}) 
+    .then(users => res.json(users))
+})
 
-// app.get("/Champions/:name", (req,res) => {
-//   Champion.find({name: req.params.name})
-//     .then(champion => res.json(champion))
-// })
+router.get("/name/:name", (req, res) => {
+  User.find({userName: req.params.name}) 
+    .then(user => res.json(user))
+})
+
+router.post("/", (req, res) => {
+  User.create(req.body)
+    .then(user => res.json(user))
+})
+
+router.delete("/name/:name", (req, res) => {
+  User.deleteOne({userName: req.params.name})
+    .then(user => res.json(user))
+})
+
+router.put("/name/:name", (req, res) => {
+  User.findOneAndUpdate(
+    {userName: req.params.name},req.body, {new: true})
+    .then(user => res.json(user))
+})
 
 export default router
